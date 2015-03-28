cd7b8542931b8cc3f14e9544aabb381da1557e67
Eddie Naff <eddienaff@gmail.com>
2015-03-27 18:36:18 -0700

    Add and delete users added

--- public/modules/customers/controllers/customers.client.controller.js
+++ public/modules/customers/controllers/customers.client.controller.js
@@ -12,6 +12,35 @@ customersApp.controller('CustomersController', ['$scope', '$stateParams', 'Authe
         // Find a list of customers
         this.customers = Customers.query();
 
+                // Open a modal window to create a single customer record
+                this.modalCreate = function(size) {
+
+                    var modalInstance = $modal.open({
+                        templateUrl: 'modules/customers/views/create-customer.client.view.html',
+                        controller: function ($scope, $modalInstance) {
+
+                            $scope.ok = function() {
+
+                                // if(createCustomerForm.$valid) {}
+                                    $modalInstance.close();
+                                
+                            };
+
+                            $scope.cancel = function() {
+                                $modalInstance.dismiss('cancel');
+                            };
+                        },
+                        size: size
+                    });
+
+                    modalInstance.result.then(function(selectedItem) {
+                    }, function() {
+                        $log.info('Modal dismissed at: ' + new Date());
+                    });
+                };
+
+            
+
         // Open a modal window to update a single customer record
         this.modalUpdate = function(size, selectedCustomer) {
 
@@ -19,6 +48,17 @@ customersApp.controller('CustomersController', ['$scope', '$stateParams', 'Authe
                 templateUrl: 'modules/customers/views/edit-customer.client.view.html',
                 controller: function ($scope, $modalInstance, customer) {
                 	$scope.customer = customer;
+
+                	$scope.ok = function() {
+
+                        // if(updateCustomerForm.$valid) {}
+    						$modalInstance.close($scope.customer);
+                        
+                	};
+
+                	$scope.cancel = function() {
+						$modalInstance.dismiss('cancel');
+                	};
                 },
                 size: size,
                 resolve: {
@@ -35,83 +75,80 @@ customersApp.controller('CustomersController', ['$scope', '$stateParams', 'Authe
             });
         };
 
+        // Remove existing Customer
+        this.remove = function(customer) {
+         if ( customer ) { 
+             customer.$remove();
+
+             for (var i in this.customers) {
+                 if (this.customers [i] === customer) {
+                     this.customers.splice(i, 1);
+                 }
+             }
+         } else {
+             this.customer.$remove(function() {
+                 });
+         }
+        };
+
     }
 ]);
 
 customersApp.controller('CustomersEditController', ['$scope', 'Customers',
-    function($scope, Customers) {}
+    function($scope, Customers) {
+
+    	// Update existing Customer
+    	this.update = function(updatedCustomer ) {
+    		var customer = updatedCustomer;
+
+    		customer.$update(function() {
+    		}, function(errorResponse) {
+    			$scope.error = errorResponse.data.message;
+    		});
+    	};
+    }
 ]);
 
-customersApp.controller('CustomersCreateController', ['$scope', 'Customers',
-    function($scope, Customers) {}
+customersApp.controller('CustomersCreateController', ['$scope', 'Customers', 'Notify',
+    function($scope, Customers, Notify) {
+
+        // Create new Customer 
+        this.create = function() {
+         // Create new Customer object
+         var customer = new Customers ({
+             firstName: this.firstName,
+             surname: this.surname,
+             subrub: this.suburb,
+             country: this.country,
+             industry: this.industry,
+             email: this.email,
+             phone: this.phone,
+             referred: this.referred,
+             channel: this.channel
+         });
+
+         // Redirect after save
+         customer.$save(function(response) {
+
+             Notify.sendMsg('NewCustomer', {'id': response._id});
+
+         }, function(errorResponse) {
+             $scope.error = errorResponse.data.message;
+         });
+        };
+    }
 ]);
 
-// // Create new Customer
-// $scope.create = function() {
-// 	// Create new Customer object
-// 	var customer = new Customers ({
-// 		firstName: this.firstName,
-// 		surname: this.surname,
-// 		subrub: this.suburb,
-// 		country: this.country,
-// 		industry: this.industry,
-// 		email: this.email,
-// 		phone: this.phone,
-// 		referred: this.referred,
-// 		channel: this.channel
-// 	});
-
-// 	// Redirect after save
-// 	customer.$save(function(response) {
-// 		$location.path('customers/' + response._id);
-
-// 		// Clear form fields
-// 		$scope.firstName = '';
-// 		$scope.surname = '';
-// 		$scope.suburb = '';
-// 		$scope.country = '';
-// 		$scope.industry = '';
-// 		$scope.email = '';
-// 		$scope.phone = '';
-// 		$scope.referred = '';
-// 		$scope.channel = '';
-
-// 	}, function(errorResponse) {
-// 		$scope.error = errorResponse.data.message;
-// 	});
-// };
-
-// // Remove existing Customer
-// $scope.remove = function(customer) {
-// 	if ( customer ) { 
-// 		customer.$remove();
-
-// 		for (var i in $scope.customers) {
-// 			if ($scope.customers [i] === customer) {
-// 				$scope.customers.splice(i, 1);
-// 			}
-// 		}
-// 	} else {
-// 		$scope.customer.$remove(function() {
-// 			$location.path('customers');
-// 		});
-// 	}
-// };
-
-// // Update existing Customer
-// $scope.update = function() {
-// 	var customer = $scope.customer;
-
-// 	customer.$update(function() {
-// 		$location.path('customers/' + customer._id);
-// 	}, function(errorResponse) {
-// 		$scope.error = errorResponse.data.message;
-// 	});
-// };
-
-// // Find existing Customer
-$scope.findOne = function() {
-	$scope.customer = Customers.get({ 
-		customerId: $stateParams.customerId
-	});
-};
+customersApp.directive('customerList', ['Customers', 'Notify', function(Customers, Notify) {
+    return {
+        restrict: 'E',
+        transclude: true,
+        templateUrl: 'modules/customers/views/customer-list-template.html',
+        link: function(scope, element, attrs) {
+            // When a new customer is added, update the customer list
+            Notify.getMsg('NewCustomer', function(event, data) {
+                scope.customersCtrl.customers = Customers.query();
+            });
+        }
+    };
+}]);
